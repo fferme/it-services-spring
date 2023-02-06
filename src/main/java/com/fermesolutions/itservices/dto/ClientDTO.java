037b36f012656c048067fa36d90af6bd99cc8af9
Felipe Cajueiro <fferme@gmail.com>
2023-02-06 19:01:36 -0300

    Inclusão de padrão DTO para entidade Cliente

---
+++ src/main/java/com/fermesolutions/itservices/dto/ClientDTO.java
@@ -0,0 +1,24 @@
+package com.fermesolutions.itservices.dto;
+
+import java.util.Set;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fermesolutions.itservices.model.Order;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record ClientDTO(
+        @JsonProperty("_id") Long id,
+        @NotBlank @NotNull @Length(min = 5, max = 40) String name,
+        @NotNull Character gender, 
+        @NotNull @Length(max = 11) @Pattern(regexp = "^[1-9][0-9]{9,10}$") String phoneNumber, 
+        @Length(max = 30) String neighbourhood, 
+        @Length(max = 20) String reference
+    ) {
+    
+}
