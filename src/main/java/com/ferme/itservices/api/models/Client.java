23c5d698705f40a98a5419ce6e1e5abc1538277e
Felipe Ferme <fferme@gmail.com>
2024-03-15 20:38:55 -0300

    Mudança para padrão builder da entidade cliente

--- src/main/java/com/ferme/itservices/api/models/Client.java
+++ src/main/java/com/ferme/itservices/api/models/Client.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.apache.commons.lang3.builder.EqualsBuilder;
@@ -20,6 +21,7 @@ import java.util.UUID;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Data
 @Entity
 @Table(name = "client")
--- src/test/java/com/ferme/itservices/common/ClientConstants.java
+++ src/test/java/com/ferme/itservices/common/ClientConstants.java
@@ -3,6 +3,18 @@ package com.ferme.itservices.common;
 import com.ferme.itservices.api.models.Client;
 
 public class ClientConstants {
-    public static final Client CLIENT = new Client("José Alves", "21983904857", "Tijuca", "Av. Maracanã 74", "Cliente");
-    public static final Client INVALID_CLIENT = new Client("", "", "", "", "");
+    public static final Client CLIENT = Client.builder()
+                                              .name("Jorge")
+                                              .phoneNumber("21988011773")
+                                              .address("Rua Aquidabã 74")
+                                              .neighborhood("Méier")
+                                              .reference("Eu")
+                                              .build();
+    public static final Client INVALID_CLIENT = Client.builder()
+                                              .name("")
+                                              .phoneNumber("")
+                                              .address("")
+                                              .neighborhood("")
+                                              .reference("")
+                                              .build();
 }
