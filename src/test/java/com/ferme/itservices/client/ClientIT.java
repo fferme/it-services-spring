b13e4e529405db4cae60af3aada224aa567446b7
Felipe Ferme <fferme@gmail.com>
2024-04-22 12:44:06 -0300

    Configuração de banco de dados SQL

--- src/test/java/com/ferme/itservices/client/ClientIT.java
+++ src/test/java/com/ferme/itservices/client/ClientIT.java
@@ -1,8 +1,10 @@
-package com.ferme.itservices;
+package com.ferme.itservices.client;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("it")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ClientIT {
 
