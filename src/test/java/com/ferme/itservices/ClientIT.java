7db77b215fefcce192d4161d303d2219f0bad411
Felipe Ferme <fferme@gmail.com>
2024-04-22 12:30:27 -0300

    Configuração servidor de aplicação

---
+++ src/test/java/com/ferme/itservices/ClientIT.java
@@ -0,0 +1,12 @@
+package com.ferme.itservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ClientIT {
+
+	@Test
+	public void contextLoads() {}
+
+}
