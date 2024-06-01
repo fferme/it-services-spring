7a3af444363ef256953379e6344e3ab9ef3897e3
Felipe Ferme <fferme@gmail.com>
2024-06-01 19:23:39 -0300

    🧪 test: Inclusão de cliente e order items no SQL da entidade order

--- src/test/resources/scripts/import_orders.sql
+++ src/test/resources/scripts/import_orders.sql
@@ -1,35 +1,40 @@
-INSERT INTO orders (id, device_name, device_sn, problems, client_id, total_price)
-VALUES (1, 'Asus Notebook AB9299', '91839021', 'Erro ao iniciar sistema', 1, 1000.00);
+INSERT INTO orders (id, device_name, device_sn, issues, client_id, total_price)
+VALUES ('e1b64829-6a03-45aa-a12f-211153c3d19c', 'Asus Notebook AB9299', '918390SDA21', 'Erro ao iniciar sistema',
+        'd6a0eb46-d011-4fb1-95ab-44f6d415d9bf', 1000.00);
 
-INSERT INTO orders (id, device_name, device_sn, problems, client_id, total_price)
-VALUES (2, 'Asus Notebook AB9499', '91233301', 'Erro ao iniciar sistema', 2, 1000.00);
+INSERT INTO orders (id, device_name, device_sn, issues, client_id, total_price)
+VALUES ('fdfe1855-8c37-4fce-bdeb-2dcd00ab2ca0', 'Asus Notebook AB9499', '91233301', 'Erro ao iniciar sistema',
+        '88e3c5f7-d0c1-4829-b5e0-61541f9aa2dc',
+        1000.00);
 
-INSERT INTO orders (id, device_name, device_sn, problems, client_id, total_price)
-VALUES (3, 'Asus Notebook AB4599', '233314445', 'Erro ao iniciar sistema', 3, 1200.00);
-
-INSERT INTO rel_order_order_items (order_id, order_item_id)
-VALUES (1, 1);
-
-INSERT INTO rel_order_order_items (order_id, order_item_id)
-VALUES (1, 2);
-
-INSERT INTO rel_order_order_items (order_id, order_item_id)
-VALUES (1, 3);
+INSERT INTO orders (id, device_name, device_sn, issues, client_id, total_price)
+VALUES ('1b4d3287-296c-4a81-9f5b-e234f5aa7aef', 'Asus Notebook AB4599', '233314445', 'Erro ao iniciar sistema',
+        '2e8d5d11-61d7-4976-b5c9-54adfea13e5b',
+        1200.00);
 
 INSERT INTO rel_order_order_items (order_id, order_item_id)
-VALUES (2, 1);
+VALUES ('e1b64829-6a03-45aa-a12f-211153c3d19c', '0def47bf-ea7e-430a-a1e9-019a1f48d0ec');
 
 INSERT INTO rel_order_order_items (order_id, order_item_id)
-VALUES (2, 2);
+VALUES ('e1b64829-6a03-45aa-a12f-211153c3d19c', '78e4961d-ee65-4a7b-b0c0-280b72654b7c');
 
 INSERT INTO rel_order_order_items (order_id, order_item_id)
-VALUES (2, 3);
+VALUES ('e1b64829-6a03-45aa-a12f-211153c3d19c', '817a23cc-8e40-487f-ae27-fed67a113413');
 
 INSERT INTO rel_order_order_items (order_id, order_item_id)
-VALUES (3, 1);
+VALUES ('fdfe1855-8c37-4fce-bdeb-2dcd00ab2ca0', '0def47bf-ea7e-430a-a1e9-019a1f48d0ec');
 
 INSERT INTO rel_order_order_items (order_id, order_item_id)
-VALUES (3, 2);
+VALUES ('fdfe1855-8c37-4fce-bdeb-2dcd00ab2ca0', '78e4961d-ee65-4a7b-b0c0-280b72654b7c');
 
 INSERT INTO rel_order_order_items (order_id, order_item_id)
-VALUES (3, 3);
+VALUES ('fdfe1855-8c37-4fce-bdeb-2dcd00ab2ca0', '817a23cc-8e40-487f-ae27-fed67a113413');
+
+INSERT INTO rel_order_order_items (order_id, order_item_id)
+VALUES ('1b4d3287-296c-4a81-9f5b-e234f5aa7aef', '0def47bf-ea7e-430a-a1e9-019a1f48d0ec');
+
+INSERT INTO rel_order_order_items (order_id, order_item_id)
+VALUES ('1b4d3287-296c-4a81-9f5b-e234f5aa7aef', '78e4961d-ee65-4a7b-b0c0-280b72654b7c');
+
+INSERT INTO rel_order_order_items (order_id, order_item_id)
+VALUES ('1b4d3287-296c-4a81-9f5b-e234f5aa7aef', '817a23cc-8e40-487f-ae27-fed67a113413');
