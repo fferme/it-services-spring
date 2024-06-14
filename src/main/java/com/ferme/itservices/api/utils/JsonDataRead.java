c529e4ec4fa549ee73f2975458225964f1985a92
fferme <fferme@gmail.com>
2024-06-14 00:49:10 -0300

    💥 fix: Parse de data no Jackson e log

--- src/main/java/com/ferme/itservices/api/utils/JsonDataRead.java
+++ src/main/java/com/ferme/itservices/api/utils/JsonDataRead.java
@@ -2,22 +2,26 @@ package com.ferme.itservices.api.utils;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.Generated;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 @Generated
+@Slf4j
 public abstract class JsonDataRead {
 	public static <T> List<T> readJsonData(String filePath, TypeReference<List<T>> typeReference) {
 		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
 		File path = new File(filePath);
 
 		try {
 			return objectMapper.readValue(path, typeReference);
 		} catch (IOException e) {
-			System.err.println("Error when reading JSON array: " + e.getMessage());
+			log.error("Error when reading JSON array: {}", e.getMessage());
 			return List.of();
 		}
 	}
